5fe045eb1c522d83a31b74aa26fb22e92e06f869
Finn Woelm <finn.woelm@gmail.com>
2016-01-03 19:23:04 -0700

    Added some extra seed data and added a seed task to Capistrano

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,27 @@ questions = Question.create([
   {title: "Food: Meat Consumption",
     question: "How many meals with meat do you eat per week?"}  
   ])
+
+reduction_options = ReductionOption.create([
+  {title: "Bike Friend",
+    description: "Bike to school once per week.",
+    carbon_impact: "250",
+    image_url: "http://www.missohiobiketour.com/wp-content/uploads/2014/02/Biking-in-Europe.jpg"},
+  {title: "Plant Muncher",
+    description: "Have only vegan meals one day per week.",
+    carbon_impact: "375",
+    image_url: "http://img2.timeinc.net/health/img/web/2014/01/slides/vegan-intro-400x400.jpg"},
+  {title: "Light It Up",
+    description: "Replace your light bulbs with energy-saving LEDs.",
+    carbon_impact: "160",
+    image_url: "http://s3.amazonaws.com/digitaltrends-uploads-prod/2015/06/Generic-LED-light-bulb.jpg"},
+  {title: "Cold As Ice",
+    description: "Wash your clothes with cold water.",
+    carbon_impact: "50",
+    image_url: "http://i.telegraph.co.uk/multimedia/archive/01850/washing-machine_1850435b.jpg"},
+   {title: "Tree Hugger",
+    description: "Plant one hectare of trees.",
+    carbon_impact: "1000",
+    image_url: "http://www.earthtimes.org/newsimage/protecting-rainforests-responsibility-conservation_18411.jpg"}, 
+  
+  ])
