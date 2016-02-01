9bd0688bea43922d28a49014fd3108832ad19063
Finn Woelm <finn.woelm@gmail.com>
2016-02-01 08:41:34 -0700

    Changed db:seed to db:reset task

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,12 +53,12 @@ namespace :puma do
 end
 
 namespace :deploy do
-  desc 'Runs rake db:seed'
+  desc 'Runs rake db:reset'
   task :seed => [:set_rails_env] do
     on primary fetch(:migration_role) do
       within release_path do
         with rails_env: fetch(:rails_env) do
-          execute :rake, "db:seed"
+          execute :rake, "db:reset"
         end
       end
     end
